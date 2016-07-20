d0e0aef2b7fcc483c1bff30974114c70ecde8f0d
ame <956083190@qq.com>
2016-07-20 17:31:54 +0800

    添加了retrofit和viewpager indicator

---
+++ app/src/main/java/hise/hznu/istudy/base/BaseActivity.java
@@ -0,0 +1,40 @@
+package hise.hznu.istudy.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by PC on 2016/7/20.
+ */
+public class BaseActivity extends FragmentActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+        initData();
+        initViews();
+    }
+    public void initData(){
+
+    }
+    public void initViews(){
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
