586094e8e59fe4ec6cc536a782b1392eedeb6467
ame <956083190@qq.com>
2016-07-21 18:26:07 +0800

    随便写了点东西

--- app/src/main/java/hise/hznu/istudy/app/AppConstant.java
+++ app/src/main/java/hise/hznu/istudy/app/AppConstant.java
@@ -4,5 +4,7 @@ package hise.hznu.istudy.app;
  * Created by PC on 2016/7/21.
  */
 public class AppConstant {
+    public static final String SERVER_URL="http://dodo.hznu.edu.cn/api/"; //服务器地址
 
+    public static final String LOGIN_ACTION = "login"; //登录
 }
--- app/src/main/java/hise/hznu/istudy/base/BaseActivity.java
+++ app/src/main/java/hise/hznu/istudy/base/BaseActivity.java
@@ -3,6 +3,7 @@ package hise.hznu.istudy.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.Window;
 
 import butterknife.ButterKnife;
 
@@ -13,6 +14,7 @@ public class BaseActivity extends FragmentActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         ButterKnife.bind(this);
         initData();
         initViews();
